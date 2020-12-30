6631de39c5724da0ac9824538cc890fedc8565c8
imhilla <hillaryodhiambo282@gmail.com>
2020-12-30 08:05:34 +0300

    fix hover issue

--- src/components/Best.js
+++ src/components/Best.js
@@ -1,6 +1,7 @@
 import image1 from '../images/image1.jpg';
 
 export default function Best(props) {
+
   return (
     <div className="mybest">
       <h1>My best</h1>
@@ -17,29 +18,31 @@ export default function Best(props) {
               color: props.currentColor.color,
             }}
           >Title</h1>
-          <div className="usedContainer">
-            <div className="used"
-              style={{
-                backgroundColor: props.reverseColor.reverse,
-                color: props.currentColor.color,
-              }}>HTML</div>
-            <div className="used"
-              style={{
-                backgroundColor: props.reverseColor.reverse,
-                color: props.currentColor.color,
-              }}>HTML</div>
-            <div className="used"
-              style={{
-                backgroundColor: props.reverseColor.reverse,
-                color: props.currentColor.color,
-              }}>HTML</div>
-            <div className="used"
-              style={{
-                backgroundColor: props.reverseColor.reverse,
-                color: props.currentColor.color,
-              }}>HTML</div>
+          <div className="hidden">
+            <div className="usedContainer">
+              <div className="used"
+                style={{
+                  backgroundColor: props.reverseColor.reverse,
+                  color: props.currentColor.color,
+                }}>HTML</div>
+              <div className="used"
+                style={{
+                  backgroundColor: props.reverseColor.reverse,
+                  color: props.currentColor.color,
+                }}>HTML</div>
+              <div className="used"
+                style={{
+                  backgroundColor: props.reverseColor.reverse,
+                  color: props.currentColor.color,
+                }}>HTML</div>
+              <div className="used"
+                style={{
+                  backgroundColor: props.reverseColor.reverse,
+                  color: props.currentColor.color,
+                }}>HTML</div>
+            </div>
+            <a href="" className="seeproject button">see the project</a>
           </div>
-          <a href="" className="seeproject button">see the project</a>
         </div>
         <div className="promotedsecond"
           style={{
