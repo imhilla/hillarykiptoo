aeddf7909fafc83d74b64df9971675733a4ae31f
imhilla <hillaryodhiambo282@gmail.com>
2021-01-05 13:06:03 +0300

    style social height

--- src/components/Social.js
+++ src/components/Social.js
@@ -12,6 +12,7 @@ export default function Social(props) {
         <a href={twitter} style={{ color: `${props.reverseColor.reverse}` }}><FaTwitter /></a>
         <a href={github} style={{ color: `${props.reverseColor.reverse}` }}><FaGithub /></a>
       </div>
+      <div className="social-height" style={{ backgroundColor: `${props.reverseColor.reverse}` }} />
       {/* <div className="contactme">
         <button className="button">Contact me</button>
         <a href="#" className="mywork button"><FaAngleDoubleDown className="downbutton bounce" />My work</a>
