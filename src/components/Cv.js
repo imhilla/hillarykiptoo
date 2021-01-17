634ded660ed70c2fc4fac50bda8c21f71ef64b35
imhilla <hillaryodhiambo282@gmail.com>
2021-01-17 14:01:31 +0300

    add view resume link

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,12 +1,15 @@
 export default function Cv(props) {
+  const uploadedFileLink = 'https://docs.google.com/document/d/13pzUnlqrqUeNMvqFoHhRyk7A0fA_yHqojTRKEKgcT0o/edit?usp=sharing'
   return (
     <div className="cv">
       <div className="pdf" style={{ color: props.currentColor.color, backgroundColor: props.reverseColor.reverse }}>PDF</div>
-      <h1 className="resumetitle" style={{ color: props.reverseColor.reverse }}>Download my Resume</h1>
+      <h1 className="resumetitle" style={{ color: props.reverseColor.reverse }}>View my Resume</h1>
       <p className="gmailpropmt" style={{ color: props.reverseColor.reverse }}>
-        {/* Lorem ipsum gretrb jhdhhdk Lorem ipsum gretrb jhdhhdk */}
-       </p>
-      <button className="downloadbutton" style={{ color: props.currentColor.color, backgroundColor: props.reverseColor.reverse }}>Download</button>
+      </p>
+      <a href={uploadedFileLink} target="_blank" rel="noopener noreferrer" download>
+        <button className="downloadbutton"
+         style={{ color: props.currentColor.color, backgroundColor: props.reverseColor.reverse }}>View CV</button>
+      </a>
     </div>
   )
 }
