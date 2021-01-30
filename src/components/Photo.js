4028a9b5ef67907f0abe5b754a945c77af184f95
imhilla <hillaryodhiambo282@gmail.com>
2021-01-30 20:21:33 +0300

    fix gif issue

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -3,7 +3,6 @@ import photoGIFWhite from '../images/gifwhite.gif'
 
 
 export default function Photo(props) {
-  console.log(props.currentColor.color);
   const image = props.currentColor.color === '#444444' ? (<img src={photoGIF} alt="React Logo" className="svgphoto" />) : (<img src={photoGIFWhite} alt="React Logo" className="svgphoto" />)
   return (
     <div className="photocontainer">
