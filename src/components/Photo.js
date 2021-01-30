84ed57e40b54f2cb592442419979cb61683a0cca
imhilla <hillaryodhiambo282@gmail.com>
2021-01-30 18:28:48 +0300

    add motion image

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,13 +1,15 @@
 import dots from '../images/dotstransparent.png'
 import photoSvg from '../images/hipic.jpg'
+import photoGIF from '../images/gifgray.gif'
+import photoGIFWhite from '../images/gifwhite.gif'
+
 
 export default function Photo(props) {
+  console.log(props.currentColor.color);
+  const image = props.currentColor.color === '#444444' ? (<img src={photoGIF} alt="React Logo" className="svgphoto" />) : (<img src={photoGIFWhite} alt="React Logo" className="svgphoto" />)
   return (
     <div className="photocontainer">
-      <img src={dots} alt="React Logo" className="dots1" />
-      <img src={dots} alt="React Logo" className="dots2" />
-      <img src={dots} alt="React Logo" className="dots3" />
-      <img src={photoSvg} alt="React Logo" className="svgphoto" />
+      {image}
     </div>
   )
 }
